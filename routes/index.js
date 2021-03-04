a1d3fed7a54805e5b89822f61568e8a44072106b
Avery Hom <akhom@ucsd.edu>
2021-03-04 10:06:09 -0800

    debugging lab

--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,17 @@
 var labs = require('../labs.json');
 
 exports.view = function(req, res){
+	console.log(labs.length);
   if(labs.length < 9) {
-  	var todaysLab = [{
+  	labs.push({
 		"id": "lab-9",
 		"title": "Debugging 101",
 		"date": "March 7",
 		"image": "debug.png"
-	}];
-
-  	labs.push(todaysLab);
-  }
+	});
+}
 
   res.render('index', {
 	'labs': labs
   });
-};
+}
